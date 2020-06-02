667d49190ab147d63b5ee05d90aa1b67fc3e861f
伯潜 <chenhe.zch@alibaba-inc.com>
2020-06-02 20:22:34 +0800

    reset

--- spring-cloud-function-samples/function-sample/src/test/java/com/example/SampleApplicationMvcTests.java
+++ spring-cloud-function-samples/function-sample/src/test/java/com/example/SampleApplicationMvcTests.java
@@ -46,6 +46,7 @@ public class SampleApplicationMvcTests {
 	public void words() throws Exception {
 		MvcResult result = this.mockMvc.perform(get("/words")).andReturn();
 		mockMvc.perform(asyncDispatch(result)).andExpect(content().string("[\"foo\",\"bar\"]"));
+		System.out.println("git reset...");
 	}
 
 	@Test
